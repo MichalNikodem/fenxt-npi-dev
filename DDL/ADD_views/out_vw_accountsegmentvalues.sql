48555646885ec52cc18d15f00ba4ddb19d46a5ca
vaclav <vaclav.rek@gooddata.com>
2018-11-13 13:59:18 +0100

    ADD mapping fix

--- DDL/ADD_views/out_vw_accountsegmentvalues.sql
+++ DDL/ADD_views/out_vw_accountsegmentvalues.sql
@@ -3,9 +3,7 @@ SELECT
 	SegmentId::VARCHAR(512) as "a__accountsegment__accountsegmentfieldid",
 	AccountSegmentValue::VARCHAR(512) as "a__accountsegment__accountsegmentvaluetext",
 	Gooddata_attr(AccountId||'#'||SegmentId)::VARCHAR(512) as "cp__accountsegment__accountsegment",
-	SegmentName::VARCHAR(512) as "l__accountsegment__accountsegmentfieldid__accountsegmentfieldid",
+	SegmentName::VARCHAR(512) as "l__accountsegment__accountsegmentfieldid__accountsegmentfieldname",
 	AccountId::VARCHAR(512) as "r__accounts",
 	TenantId::VARCHAR(128) as x__client_id
-FROM stg_csv_AccountSegmentValue_merge
--- where _sys_transform_id = (select max(id) from _sys_transform_id where ts_end is not null and entity = 'dm_TBD')
-;
+FROM stg_csv_AccountSegmentValue_merge;
