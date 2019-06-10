dc15e1fae975b47838b0f40fd552c69880f39918
Michal Nikodem <michal.nikodem@gooddata.com>
2019-06-10 14:46:52 +0200

    Project Budgets correct grain and adding order to group 6

--- DDL/ADD_views/out_vw_projectbudgets.sql
+++ DDL/ADD_views/out_vw_projectbudgets.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE VIEW out_vw_projectbudgets AS
 SELECT
-	(coalesce(ProjectBudgetId,'0')||'-'||coalesce(AccountId,'#')||'-'||FiscalPeriodId||'-'||ScenarioId)::VARCHAR(512) as "cp__projectbudgetid",
+	(coalesce(ProjectId,'0')||'-'||coalesce(AccountId,'#')||'-'||FiscalPeriodId||'-'||ScenarioId)::VARCHAR(512) as "cp__projectbudgetid",
 	PeriodAmount::NUMERIC(17,2) as "f__periodamount",
 	AccountId::VARCHAR(512) as "r__accounts",
 	FiscalPeriodId::VARCHAR(512) as "r__fiscalperiods",
--- DDL/out_ProjectBudgets.sql
+++ DDL/out_ProjectBudgets.sql
@@ -6,7 +6,6 @@ CREATE TABLE out_ProjectBudgets
 (
     TenantId varchar(255) encoding rle,
     PeriodAmount varchar(255),
-    ProjectBudgetId varchar(255),
     ProjectId varchar(255),
     AccountId varchar(255),
     FiscalPeriodId varchar(255),
@@ -14,15 +13,15 @@ CREATE TABLE out_ProjectBudgets
     _sys_is_deleted BOOLEAN encoding rle,
     _sys_hash varchar(32),
     _sys_updated_at timestamp
-) ORDER BY AccountId,
+) ORDER BY TenantId,
+           AccountId,
            FiscalPeriodId,
-           ProjectBudgetId,
+           ProjectId,
            ScenarioId,
-           TenantId,
            _sys_hash,
            _sys_is_deleted
 
-SEGMENTED BY hash(TenantId,AccountId,FiscalPeriodId,ProjectBudgetId,ScenarioId) ALL NODES;
+SEGMENTED BY hash(TenantId,AccountId,FiscalPeriodId,ProjectId,ScenarioId) ALL NODES;
 
 CREATE TABLE wrk_out_ProjectBudgets LIKE out_ProjectBudgets INCLUDING PROJECTIONS;
 CREATE TABLE wrk_out_ProjectBudgets_diff LIKE out_ProjectBudgets INCLUDING PROJECTIONS;
